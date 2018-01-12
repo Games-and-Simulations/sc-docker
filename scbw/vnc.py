ccde57ad0b214f1603ab6a0c502a293e7ad35236
Javier Sacido <100316452@alumnos.uc3m.es>
2018-01-12 15:31:53 +0100

    Added vnc-viewer support for Windows
    Now also works for windows (tested in Win10).

--- scbw/vnc.py
+++ scbw/vnc.py
@@ -1,17 +1,22 @@
 import subprocess
-
-from .utils import which
-
+from os import environ, sys
+from utils import which
 
 def check_vnc_exists():
-    try:
-        out = which("vnc-viewer")
-    except Exception as e:
-        raise Exception("An error occurred while trying to find path to vnc-viewer")
-
+    if(sys.platform == "win32"):
+        if "RealVNC" in environ.get('Path'):
+            out = "vncviewer"
+    else:
+        try:
+            out = which("vnc-viewer")
+        except Exception as e:
+            raise Exception("An error occurred while trying to find path to vnc-viewer")
+    
     if out is None:
         raise Exception(f"vnc-viewer not found!")
 
-
 def launch_vnc_viewer(port: int):
-    subprocess.call(f"vnc-viewer localhost:{port} &", shell=True)
+    if(sys.platform == "win32"):
+        subprocess.call(f"vncviewer localhost:{port} &", shell=True)
+    else:
+        subprocess.call(f"vnc-viewer localhost:{port} &", shell=True)
