2c866850eb9146bf33a4f6f75991a05e21fb1590
Javier Sacido <100316452@alumnos.uc3m.es>
2018-01-12 15:46:17 +0100

    Updated to latest dev changes

--- scbw/vnc.py
+++ scbw/vnc.py
@@ -1,11 +1,13 @@
 import subprocess
 from os import environ, sys
-from utils import which
+from .utils import which
 
 def check_vnc_exists():
     if(sys.platform == "win32"):
         if "RealVNC" in environ.get('Path'):
             out = "vncviewer"
+        else:
+            out = None
     else:
         try:
             out = which("vnc-viewer")
@@ -20,3 +22,4 @@ def launch_vnc_viewer(port: int):
         subprocess.call(f"vncviewer localhost:{port} &", shell=True)
     else:
         subprocess.call(f"vnc-viewer localhost:{port} &", shell=True)
+
