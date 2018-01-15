48f3b9680902388c38cc539c999c8a35d0a2e884
Michal Sustr <michal.sustr@blindspot.ai>
2018-01-15 11:27:32 +0100

    add some +x for easier debugging

--- docker/entrypoints/play_human.sh
+++ docker/entrypoints/play_human.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eu
+set -eux
 
 # human is always headful
 IS_HEADFUL="1"
--- docker/entrypoints/wine_entrypoint.sh
+++ docker/entrypoints/wine_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eu
+set -eux
 
 Xvfb :0 -auth ~/.Xauthority -screen 0 640x480x24 >> ${LOG_DIR}/xvfb.log 2>&1 &
 x11vnc -forever -nopw -display :0 >> ${LOG_DIR}/xvnc.log 2>&1 &
