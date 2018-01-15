e84113453ab473d749b17b7b3182bca0ef80d39b
Michal Sustr <michal.sustr@blindspot.ai>
2018-01-15 15:23:49 +0100

    create new python version

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -eux
 
-VERSION=0.1
+VERSION="-0.2a3"
 
-docker tag starcraft:wine  ggaic/starcraft:wine-$VERSION
-docker tag starcraft:bwapi ggaic/starcraft:bwapi-$VERSION
-docker tag starcraft:java  ggaic/starcraft:java-$VERSION
-docker tag starcraft:play  ggaic/starcraft:play-$VERSION
+docker tag starcraft:wine  ggaic/starcraft:wine${VERSION}
+docker tag starcraft:bwapi ggaic/starcraft:bwapi${VERSION}
+docker tag starcraft:java  ggaic/starcraft:java${VERSION}
+docker tag starcraft:play  ggaic/starcraft:play${VERSION}
 
-docker push ggaic/starcraft:wine-$VERSION
-docker push ggaic/starcraft:bwapi-$VERSION
-docker push ggaic/starcraft:java-$VERSION
-docker push ggaic/starcraft:play-$VERSION
+docker push ggaic/starcraft:wine${VERSION}
+docker push ggaic/starcraft:bwapi${VERSION}
+docker push ggaic/starcraft:java${VERSION}
+docker push ggaic/starcraft:play${VERSION}
 
