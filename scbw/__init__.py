445e66463d5172457566bf5e2e26b8c931a0a480
Michal Sustr <michal.sustr@blindspot.ai>
2018-01-17 11:25:08 +0100

    Update package __all__

--- scbw/__init__.py
+++ scbw/__init__.py
@@ -1,6 +1,6 @@
-from .game import run_game, GameResult
+from .game import run_game, GameResult, GameArgs
 
 VERSION = "0.2a9"
 
 # You shouldn't need to use anything else other than these files:
-__all__ = ['run_game', GameResult]
+__all__ = ['run_game', 'GameResult', 'GameArgs']
