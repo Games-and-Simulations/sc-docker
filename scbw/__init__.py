f6ece7c772b05d5f527d10a51619fff9fa2b1667
Michal Sustr <michal.sustr@blindspot.ai>
2018-01-18 10:43:48 +0100

    Update version

--- scbw/__init__.py
+++ scbw/__init__.py
@@ -1,7 +1,7 @@
 from .game import run_game, GameResult, GameArgs, GameException
 from .docker import DockerException
 
-VERSION = "0.2a9"
+VERSION = "0.2a10"
 
 # You shouldn't need to use anything else other than these:
 __all__ = ['run_game', 'GameResult', 'GameArgs', 'GameException', 'DockerException']
