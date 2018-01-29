84690b142882fd8b6cfc5a21a0f8e780b186c35e
michalsustr <michal.sustr@aic.fel.cvut.cz>
2018-01-29 16:50:26 +0100

    Make project a beta

--- scbw/__init__.py
+++ scbw/__init__.py
@@ -1,7 +1,7 @@
 from .game import run_game, GameResult, GameArgs
 from .error import DockerException, GameException
 
-VERSION = "0.2a23"
+VERSION = "0.2b"
 
 # You shouldn't need to use anything else other than these:
 __all__ = ['run_game', 'GameResult', 'GameArgs', 'GameException', 'DockerException']
