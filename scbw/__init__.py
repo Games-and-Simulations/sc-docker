f985f1afb1acc433b97c56dc071c2c2ae6c1d4b9
michalsustr <michal.sustr@aic.fel.cvut.cz>
2018-02-16 18:59:00 +0100

    Converted 8bb2e827eee65c61b0aa0da5662440a9979d54b9 to py3.4

--- scbw/__init__.py
+++ scbw/__init__.py
@@ -1,7 +1,7 @@
 from .defaults import VERSION
 from .game import run_game, GameResult, GameArgs
 from .error import DockerException, GameException
-
-
-# You shouldn't need to use anything else other than these:
-__all__ = ['VERSION', 'run_game', 'GameResult', 'GameArgs', 'GameException', 'DockerException']
+__all__ = [
+    'VERSION', 'run_game', 'GameResult', 'GameArgs', 'GameException',
+    'DockerException'
+]
