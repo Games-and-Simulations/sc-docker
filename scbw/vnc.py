0bf95ef78fb6322628dc5fa1416d5f415f45bd7b
michalsustr <michal.sustr@aic.fel.cvut.cz>
2018-02-20 10:20:45 +0100

    Converted bb3f1d1b3105b2ad06798fb23745ccf96710ce61 to py3.4

--- scbw/vnc.py
+++ scbw/vnc.py
@@ -1,18 +1,18 @@
 import shutil
-
 import os
 
 
 def check_vnc_exists():
     try:
-        out = shutil.which("vnc-viewer")
+        out = shutil.which('vnc-viewer')
     except Exception as e:
-        raise Exception("An error occurred while trying to find path to vnc-viewer")
+        raise Exception(
+            'An error occurred while trying to find path to vnc-viewer')
+    if (out is None):
+        raise Exception(('vnc-viewer not found!' % ()))
 
-    if out is None:
-        raise Exception(f"vnc-viewer not found!")
 
-
-def launch_vnc_viewer(host: str, port: int):
-    # launch in bg
-    os.spawnl(os.P_NOWAIT, shutil.which("vnc-viewer"), "vnc-viewer", f"{host}:{port}")
+def launch_vnc_viewer(host, port):
+    os.spawnl(os.P_NOWAIT,
+              shutil.which('vnc-viewer'), 'vnc-viewer', ('%s:%s' % (host,
+                                                                    port)))
