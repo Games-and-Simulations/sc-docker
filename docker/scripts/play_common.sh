29963e71acca842e2be352c4621ef9bcc006cd9a
michalsustr <michal.sustr@aic.fel.cvut.cz>
2018-03-29 15:11:32 +0200

    Refactor reg keys to be more readable

--- docker/scripts/play_common.sh
+++ docker/scripts/play_common.sh
@@ -211,45 +211,38 @@ function detect_game_finished() {
 
 function update_registry() {
     # disable splash screen
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v Gamma /t REG_DWORD /d 0000008c
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v ColorCycle /t REG_DWORD /d 00000001
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v UnitPortraits /t REG_DWORD /d 00000002
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v speed /t REG_DWORD /d 00000006
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v mscroll /t REG_DWORD /d 00000001
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v kscroll /t REG_DWORD /d 00000001
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v m_mscroll /t REG_DWORD /d 00000003
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v m_kscroll /t REG_DWORD /d 00000003
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v music /t REG_DWORD /d 0000002d
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v sfx /t REG_DWORD /d 0000001e
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v tipnum /t REG_DWORD /d 00000001
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v intro /t REG_DWORD /d 00000200
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v introX /t REG_DWORD /d 00000000
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v unitspeech /t REG_DWORD /d 00000001
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v unitnoise /t REG_DWORD /d 00000002
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v bldgnoise /t REG_DWORD /d 00000004
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v tip /t REG_DWORD /d 00000100
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v trigtext /t REG_DWORD /d 00000400
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v StarEdit /t REG_DWORD /d "Z:\app\sc\StarEdit.exe"
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v "Recent Maps"  /t REG_EXPAND_SZ /d ""
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v Retail /t REG_EXPAND_SZ /d "y"
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v Brood  /t REG_EXPAND_SZ /d "y"
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v StarCD /t REG_EXPAND_SZ /d ""
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v InstallPath /t REG_EXPAND_SZ /d "Z:\app\sc\\"
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft" /v Program /t REG_EXPAND_SZ /d "Z:\app\sc\StarCraft.exe"
+    REG_KEY="HKEY_LOCAL_MACHINE\SOFTWARE\Blizzard Entertainment\Starcraft"
+    wine REG ADD ${REG_KEY} /v Gamma /t REG_DWORD /d 0000008c
+    wine REG ADD ${REG_KEY} /v ColorCycle /t REG_DWORD /d 00000001
+    wine REG ADD ${REG_KEY} /v UnitPortraits /t REG_DWORD /d 00000002
+    wine REG ADD ${REG_KEY} /v speed /t REG_DWORD /d 00000006
+    wine REG ADD ${REG_KEY} /v mscroll /t REG_DWORD /d 00000001
+    wine REG ADD ${REG_KEY} /v kscroll /t REG_DWORD /d 00000001
+    wine REG ADD ${REG_KEY} /v m_mscroll /t REG_DWORD /d 00000003
+    wine REG ADD ${REG_KEY} /v m_kscroll /t REG_DWORD /d 00000003
+    wine REG ADD ${REG_KEY} /v music /t REG_DWORD /d 0000002d
+    wine REG ADD ${REG_KEY} /v sfx /t REG_DWORD /d 0000001e
+    wine REG ADD ${REG_KEY} /v tipnum /t REG_DWORD /d 00000001
+    wine REG ADD ${REG_KEY} /v intro /t REG_DWORD /d 00000200
+    wine REG ADD ${REG_KEY} /v introX /t REG_DWORD /d 00000000
+    wine REG ADD ${REG_KEY} /v unitspeech /t REG_DWORD /d 00000001
+    wine REG ADD ${REG_KEY} /v unitnoise /t REG_DWORD /d 00000002
+    wine REG ADD ${REG_KEY} /v bldgnoise /t REG_DWORD /d 00000004
+    wine REG ADD ${REG_KEY} /v tip /t REG_DWORD /d 00000100
+    wine REG ADD ${REG_KEY} /v trigtext /t REG_DWORD /d 00000400
+    wine REG ADD ${REG_KEY} /v StarEdit /t REG_DWORD /d "Z:\app\sc\StarEdit.exe"
+    wine REG ADD ${REG_KEY} /v "Recent Maps"  /t REG_EXPAND_SZ /d ""
+    wine REG ADD ${REG_KEY} /v Retail /t REG_EXPAND_SZ /d "y"
+    wine REG ADD ${REG_KEY} /v Brood  /t REG_EXPAND_SZ /d "y"
+    wine REG ADD ${REG_KEY} /v StarCD /t REG_EXPAND_SZ /d ""
+    wine REG ADD ${REG_KEY} /v InstallPath /t REG_EXPAND_SZ /d "Z:\app\sc\\"
+    wine REG ADD ${REG_KEY} /v Program /t REG_EXPAND_SZ /d "Z:\app\sc\StarCraft.exe"
 
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Blizzard Entertainment\Starcraft\DelOpt0" /v File0 /t REG_EXPAND_SZ /d "spc"
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Blizzard Entertainment\Starcraft\DelOpt0" /v File1 /t REG_EXPAND_SZ /d "mpc"
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Blizzard Entertainment\Starcraft\DelOpt0" /v Path0 /t REG_EXPAND_SZ /d "Z:\app\sc\characters"
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Blizzard Entertainment\Starcraft\DelOpt0" /v Path1 /t REG_EXPAND_SZ /d "Z:\app\sc\characters"
-
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Blizzard Entertainment\Starcraft\DelOpt1" /v File0 /t REG_EXPAND_SZ /d "sng"
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Blizzard Entertainment\Starcraft\DelOpt1" /v File1 /t REG_EXPAND_SZ /d "mlt"
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Blizzard Entertainment\Starcraft\DelOpt1" /v File2 /t REG_EXPAND_SZ /d "snx"
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Blizzard Entertainment\Starcraft\DelOpt1" /v File3 /t REG_EXPAND_SZ /d "mlx"
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Blizzard Entertainment\Starcraft\DelOpt1" /v Path0 /t REG_EXPAND_SZ /d "Z:\app\sc\Save"
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Blizzard Entertainment\Starcraft\DelOpt1" /v Path1 /t REG_EXPAND_SZ /d "Z:\app\sc\Map\Save"
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Blizzard Entertainment\Starcraft\DelOpt1" /v Path2 /t REG_EXPAND_SZ /d "Z:\app\sc\Save"
-    wine REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Blizzard Entertainment\Starcraft\DelOpt1" /v Path3 /t REG_EXPAND_SZ /d "Z:\app\sc\Maps\Save"
+    REG_KEY="HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Blizzard Entertainment\Starcraft\DelOpt0"
+    wine REG ADD ${REG_KEY} /v File0 /t REG_EXPAND_SZ /d "spc"
+    wine REG ADD ${REG_KEY} /v File1 /t REG_EXPAND_SZ /d "mpc"
+    wine REG ADD ${REG_KEY} /v Path0 /t REG_EXPAND_SZ /d "Z:\app\sc\characters"
+    wine REG ADD ${REG_KEY} /v Path1 /t REG_EXPAND_SZ /d "Z:\app\sc\characters"
 }
 
 function launch_multiplayer() {
