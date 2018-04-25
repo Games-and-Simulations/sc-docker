2e989173c18d061ac73d0e1e9218a638892e4fb2
michalsustr <michal.sustr@aic.fel.cvut.cz>
2018-04-25 17:35:25 +0200

    Converted 43a8f0568e83262c1c1e4d864c4a37286fb54523 to py3.4

--- scbw/bot_factory.py
+++ scbw/bot_factory.py
@@ -1,32 +1,21 @@
-from typing import List, Iterable
-
 from scbw.bot_storage import BotStorage
 from scbw.player import BotPlayer, PlayerRace
 
 
-
-def retrieve_bots(
-    bot_specs: Iterable[str],
-    bot_storages: Iterable[BotStorage]
-) -> List[BotPlayer]:
+def retrieve_bots(bot_specs, bot_storages):
     bots = []
     for bot_spec in bot_specs:
-        parts = bot_spec.split(":")
+        parts = bot_spec.split(':')
         bot_name = parts[0]
-
         bot = None
         for bot_storage in bot_storages:
             maybe_bot = bot_storage.find_bot(bot_name)
             if maybe_bot:
                 bot = maybe_bot
                 break
-
-        if bot is None:
-            raise Exception(f"Could not find bot {bot_name}")
-
-        if len(parts) == 2:
+        if (bot is None):
+            raise Exception(('Could not find bot %s' % (bot_name, )))
+        if (len(parts) == 2):
             bot.race = PlayerRace(parts[1])
-
         bots.append(bot)
-
     return bots
