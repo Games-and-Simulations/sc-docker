678733fa5a0afb777c0df914abf7134989c69905
michalsustr <michal.sustr@aic.fel.cvut.cz>
2018-05-08 10:28:55 +0200

    Update version

--- scbw/defaults.py
+++ scbw/defaults.py
@@ -10,7 +10,7 @@ def get_data_dir() -> str:
         return os.path.expanduser("~") + "/.scbw"
 
 
-VERSION = "1.0.1"
+VERSION = "1.0.2"
 
 SCBW_BASE_DIR = get_data_dir()
 SC_GAME_DIR = f"{SCBW_BASE_DIR}/games"
