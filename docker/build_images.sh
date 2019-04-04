fcd7992136caae13c976c70a5fc884ce58e65c8e
Bytekeeper <therealdante@gmail.com>
2019-04-04 17:38:58 +0200

    Revert "Removed useless build_args :)"
    This reverts commit efd831cf

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -eux
 
-docker build -f dockerfiles/wine.dockerfile  -t starcraft:wine   .
-docker build -f dockerfiles/bwapi.dockerfile -t starcraft:bwapi  .
-docker build -f dockerfiles/play.dockerfile  -t starcraft:play   .
-docker build -f dockerfiles/java.dockerfile  -t starcraft:java   .
+BUILD_ARGS="--build-arg BOT_UID=${BOT_UID:-1000} --build-arg STARCRAFT_UID=${STARCRAFT_UID:-1001}"
+
+docker build ${BUILD_ARGS} -f dockerfiles/wine.dockerfile  -t starcraft:wine   .
+docker build ${BUILD_ARGS} -f dockerfiles/bwapi.dockerfile -t starcraft:bwapi  .
+docker build ${BUILD_ARGS} -f dockerfiles/play.dockerfile  -t starcraft:play   .
+docker build ${BUILD_ARGS} -f dockerfiles/java.dockerfile  -t starcraft:java   .
 
 pushd ../scbw/local_docker
 [ ! -f starcraft.zip ] && curl -SL 'http://files.theabyss.ru/sc/starcraft.zip' -o starcraft.zip
-docker build -f game.dockerfile  -t "starcraft:game" .
+docker build ${BUILD_ARGS} -f game.dockerfile  -t "starcraft:game" .
 popd
