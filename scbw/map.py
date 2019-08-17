b63f23533b0498e152c75a6eef80ac88992b3a85
Bytekeeper <therealdante@gmail.com>
2019-08-17 16:30:32 +0200

    Path fix.

--- scbw/map.py
+++ scbw/map.py
@@ -30,6 +30,10 @@ def download_season_maps(map_dir: str) -> None:
     download_extract_zip(
         "https://github.com/Bytekeeper/sc-docker/releases/download/Maps_2019Season1/2019Season1.zip", map_dir
     )
+    logger.info("downloading maps for 2019 season 2")
+    download_extract_zip(
+        "https://github.com/Bytekeeper/sc-docker/releases/download/Maps_2019Season2/2019Season2.zip", map_dir
+    )
 
 
 def download_bwta_caches(bwta_dir: str, bwta2_dir: str) -> None:
@@ -49,5 +53,5 @@ def download_bwta_caches(bwta_dir: str, bwta2_dir: str) -> None:
         if not os.path.exists(f"{bwta_dir}/{file}"):
             shutil.move(tmp_dir + "/bwapi-data/BWTA/" + file, bwta_dir)
     for file in os.listdir(tmp_dir + "/bwapi-data/BWTA2"):
-        if not os.path.exists(f"{bwta_dir}/{file}"):
+        if not os.path.exists(f"{bwta2_dir}/{file}"):
             shutil.move(tmp_dir + "/bwapi-data/BWTA2/" + file, bwta2_dir)
